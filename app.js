c958047c9d413bbab28ca358beb68a867f402fb2
Katherine J. Duarte <katherine.duarte@centre.edu>
2019-10-10 11:00:42 -0400

    done

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+var setBanner= function (message){
+    
+ d3.select("#banner").text(message);}
 
+// Create a promise 
+var filmPromise= d3.json("https://ghibliapi.herokuapp.com/films")
+//Function works 
+    d
+//Functin doesn't work
+
+//
